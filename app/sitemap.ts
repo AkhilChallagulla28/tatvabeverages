c10e464f9633f4692bf53448f707769b41d8e800
Lakshmi Akhil Challagulla <akhilchallagulla28@gmail.com>
2026-02-13 21:45:24 +0530

    updated sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -24,6 +24,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/manufacturer`,
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
   ];
 }
 
